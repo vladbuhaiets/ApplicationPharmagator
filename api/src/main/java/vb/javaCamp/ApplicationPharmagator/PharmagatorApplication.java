3d40d3e810530729e23dee1f1021e341a4d2f61a
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-17 10:32:04 +0200

    Initial skeleton

---
+++ api/src/main/java/vb/javaCamp/ApplicationPharmagator/PharmagatorApplication.java
@@ -0,0 +1,13 @@
+package vb.javaCamp.ApplicationPharmagator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmagatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmagatorApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/vb/javaCamp/ApplicationPharmagator/PharmagatorApplicationTests.java
@@ -0,0 +1,13 @@
+package vb.javaCamp.ApplicationPharmagator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmagatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
