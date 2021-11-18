24aa4446b1c484a07006d4d65dff1cbd8670e352
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-18 19:32:17 +0200

    Add entities

---
+++ api/src/main/java/vb/javaCamp/pharmagator/entities/PriceId.java
@@ -0,0 +1,21 @@
+package vb.javaCamp.pharmagator.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceId implements Serializable {
+
+    @Id
+    private Long priceId;
+
+    @Id
+    private Long medicineId;
+
+}
