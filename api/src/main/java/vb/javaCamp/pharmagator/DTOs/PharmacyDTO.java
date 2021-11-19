6fd80e2a19ffd403f2e3e0d307cc849c4c3b6106
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-19 16:12:59 +0200

    Modify PharmacyDTO

--- api/src/main/java/vb/javaCamp/pharmagator/DTOs/PharmacyDTO.java
+++ api/src/main/java/vb/javaCamp/pharmagator/DTOs/PharmacyDTO.java
@@ -1,10 +1,12 @@
 package vb.javaCamp.pharmagator.DTOs;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class PharmacyDTO {
 
     private Long Id;
@@ -12,29 +14,5 @@ public class PharmacyDTO {
     private String name;
 
     private String medicineLinkTemplate;
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMedicineLinkTemplate() {
-        return medicineLinkTemplate;
-    }
-
-    public void setMedicineLinkTemplate(String medicineLinkTemplate) {
-        this.medicineLinkTemplate = medicineLinkTemplate;
-    }
 
 }
