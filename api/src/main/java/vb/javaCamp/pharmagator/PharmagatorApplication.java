3c41e5f4bda82087085a83bccbb4879c90cffa6e
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-22 12:59:27 +0200

    Add scheduler

--- api/src/main/java/vb/javaCamp/pharmagator/PharmagatorApplication.java
+++ api/src/main/java/vb/javaCamp/pharmagator/PharmagatorApplication.java
@@ -2,8 +2,10 @@ package vb.javaCamp.pharmagator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PharmagatorApplication {
 
 	public static void main(String[] args) {
