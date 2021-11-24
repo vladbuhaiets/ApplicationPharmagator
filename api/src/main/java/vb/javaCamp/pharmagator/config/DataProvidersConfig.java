3b5828afa0ce456e83c5420d0e8de8092f4171ee
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-24 11:33:01 +0200

    Add config for data providers

---
+++ api/src/main/java/vb/javaCamp/pharmagator/config/DataProvidersConfig.java
@@ -0,0 +1,37 @@
+package vb.javaCamp.pharmagator.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class DataProvidersConfig {
+
+    @Value("${pharmagator.data-providers.apteka-ds.url}")
+    private String pharmacyDSBaseUrl;
+
+    @Value("${pharmagator.data-providers.anc.url}")
+    private String pharmacyANCBaseUrl;
+
+    @Bean(name = "pharmacyDSWebClient")
+    public WebClient pharmacyDSWebClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(pharmacyDSBaseUrl)
+                .build();
+    }
+
+    @Bean(name = "pharmacyANCWebClient")
+    public WebClient pharmacyANCWebClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(pharmacyANCBaseUrl)
+                .build();
+    }
+
+}
