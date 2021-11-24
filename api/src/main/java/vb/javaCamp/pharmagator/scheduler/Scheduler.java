733cd84b5fbe05272ca0be36892e46fb4a63a8ee
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-24 11:34:19 +0200

    Modify type for data providers in scheduler

--- api/src/main/java/vb/javaCamp/pharmagator/scheduler/Scheduler.java
+++ api/src/main/java/vb/javaCamp/pharmagator/scheduler/Scheduler.java
@@ -1,7 +1,5 @@
 package vb.javaCamp.pharmagator.scheduler;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,25 +8,29 @@ import vb.javaCamp.pharmagator.DTOs.MedicineDTO;
 import vb.javaCamp.pharmagator.dataProviders.DataProvider;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class Scheduler {
 
-    private DataProvider dataProvider;
+    private final List<DataProvider> dataProviders;
 
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
     public void schedule() {
 
         log.info("Scheduler start at {}", Instant.now());
-        dataProvider.loadData().forEach(this::storeToDatabase);
+        dataProviders.parallelStream()
+                .flatMap(DataProvider::loadData)
+                .forEach(this::storeToDatabase);
 
     }
 
     private void storeToDatabase(MedicineDTO dto) {
 
-
+        log.info(dto.getTitle() + " " + dto.getExternalId() + " " + dto.getPharmacyName() + " " + dto.getPrice());
 
     }
 
