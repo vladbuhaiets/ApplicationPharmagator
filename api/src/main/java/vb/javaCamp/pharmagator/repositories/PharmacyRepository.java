47651941c19aece6e01030116d9787e60f107282
vladbuhaiets <vbuhaiets@gmail.com>
2021-11-30 19:47:45 +0200

    Add new method

--- api/src/main/java/vb/javaCamp/pharmagator/repositories/PharmacyRepository.java
+++ api/src/main/java/vb/javaCamp/pharmagator/repositories/PharmacyRepository.java
@@ -1,10 +1,17 @@
 package vb.javaCamp.pharmagator.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vb.javaCamp.pharmagator.entities.Pharmacy;
+import vb.javaCamp.pharmagator.projections.PharmacyProjection;
+
+import java.util.List;
 
 @Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+    @Query("SELECT pharmacy FROM Pharmacy pharmacy")
+    List<PharmacyProjection> findAllProj();
 
 }
